33ede63e6963296f68ca9bc4463cf473869ab0c6
daia_ <daiane.pguni@gmail.com>
2023-01-31 19:17:12 -0300

    setting up the ui

--- app/src/main/java/br/com/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/br/com/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,24 @@ package br.com.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.flStart.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will start the exercise.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
