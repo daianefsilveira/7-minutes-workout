94b0b55183e74310baf86c5857bff56bfc433c32
daia_ <daiane.pguni@gmail.com>
2023-02-01 19:19:44 -0300

    using countdowntimers to display the seconds remaining

--- app/src/main/java/br/com/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/br/com/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package br.com.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import br.com.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +26,69 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    /**
+     * Function used to set the timer for REST.
+     */
+    private fun setupRestView() {
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        // Function used to set the progress details.
+        setRestProgressBar()
+    }
+
+    /**
+     * Function used to set the progress of timer using the progress
+     */
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress // Sets the current progress to the specified value.
+
+        /**
+         * @param millisInFuture The number of millis in the future from the call
+         *   to {#start()} until the countdown is done and {#onFinish()}
+         *   is called.
+         * @param countDownInterval The interval along the way to receive
+         *   {#onTick(long)} callbacks.
+         */
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress // Indicates progress bar progress
+                binding?.tvTimer?.text = (10 - restProgress).toString() // Current progress is set to text view in terms of seconds.
+            }
+            override fun onFinish() {
+                // When the 10 seconds will complete this will be executed.
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    // Destroying the timer when closing the activity or app.
+    /**
+     * Here in the Destroy function we will reset the rest timer if it is running.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
