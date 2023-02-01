58f464d70d3c141c38919a6a1626da0951d61dbd
daia_ <daiane.pguni@gmail.com>
2023-02-01 14:55:02 -0300

    adding the exercise screen, intents and customizing the toolbar

--- app/src/main/java/br/com/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/br/com/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,19 +8,22 @@ import br.com.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        binding.flStart.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Here we will start the exercise.",
-                Toast.LENGTH_SHORT
-            ).show()
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
